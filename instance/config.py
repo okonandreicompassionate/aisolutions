6b876d88ccb24f11230f8703a40a8397c3bdfe09
Andrei Okon <okoncompassionate@gmail.com>
2025-05-21 12:53:29 +0100

    added a sql new connection

--- instance/config.py
+++ instance/config.py
@@ -1,5 +1,5 @@
 ADMIN_EMAIL="blogger@blogger.com"
 SECRET_KEY="59InUNHh4Yqh2yzW_ieqv5CBOJnmW0Z73E2hZhPT8Ss"
 ADMIN_NAME="james henry"
-SQLALCHEMY_DATABASE_URI="mysql+mysqlconnector://root@localhost:3306/aisolutions"
+SQLALCHEMY_DATABASE_URI="postgresql://neondb_owner:npg_BMHh6Dsgl0Wx@ep-red-thunder-a5psdj0v-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require"
 SQLALCHEMY_TRACK_MODIFICATIONS=False
